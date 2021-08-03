14b236561bac2a321b1d1ba35f77080d1735b62f
Jorge Soltec <jdeleon@corposoltec.com>
2021-08-03 15:24:24 -0600

    se agregó el modulo datos

--- modulos/app.js
+++ modulos/app.js
@@ -1,3 +1,12 @@
-require('./datos');
 
-console.log(global.test);
+//variable para cargar los metodos de datos.js, mejor cargarlo sobre una constante
+const datos = require('./datos');
+
+//ya se puede hacer llamada al metodo exportado por datos.js
+datos.log("Hola Mundo");
+
+console.log(module);
+/*console.log(global.test);
+
+//mala practica usar variables globales
+console.log(module);*/
--- modulos/datos.js
+++ modulos/datos.js
@@ -1,1 +1,15 @@
-global.test = "Hola mundo!"
+var url = 'http://jomadelema.com/datos'
+
+
+function dato(mensaje){
+    //envíe HTTP reuqest
+    console.log(mensaje);
+}
+
+//estos se vuelven publicos, si no está en export, es una función o dato privado. 
+module.exports.log = dato;
+
+//module.exports.url = url;
+
+
+
