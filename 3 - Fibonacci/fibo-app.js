76e232adaa8aed74ce61582fdd88f073e1fc5af0
JOMADELEMA <jomadelema@gmail.com>
2021-08-04 21:19:49 -0600

    se separo funcionalidad y llamada para la serie fibonacci

--- 3 - Fibonacci/fibo-app.js
+++ 3 - Fibonacci/fibo-app.js
@@ -1,24 +1,7 @@
-//serie fibonacci
-//1 1 2 3 5 8 13 21 34 55 89 ...
+const serie = require('./serie');
 
-const fs = require('fs');
+let cantidad = 10;
 
-let fibo1 = 1;
-let fibo2 = 1;
-let serie = '';
-
-serie += fibo1 +"\t";
-
-for(let i=2; i<=7; i++){
-    serie += fibo2 + "\t";
-    fibo2 = fibo1 + fibo2;
-    fibo1 = fibo2 - fibo1;
-};
-
-//instalar nodemon con: npm install -g nodemon
-
-//a continuación se generará código para guardar el resultado en un archivo
-fs.writeFile('fibonacci.txt', serie, (err)=>{
-    if (err) throw err;
-    console.log('Archivo creado con exito!');
-});
+serie.crearSerie(cantidad)
+    .then(mensaje => console.log (mensaje))
+    .catch(mensaje => console.log(mensaje));
---
+++ 3 - Fibonacci/serie.js
@@ -0,0 +1,35 @@
+//serie fibonacci
+//1 1 2 3 5 8 13 21 34 55 89 ...
+
+const fs = require('fs');
+
+let crearSerie = (cantidad) => {
+
+    return new Promise((resolve, reject)=> {
+        let fibo1 = 1;
+        let fibo2 = 1;
+        let serie = '';
+        
+        serie += fibo1 +"\t";
+        
+        for(let i=2; i<cantidad; i++){
+            serie += fibo2 + "\t";
+            fibo2 = fibo1 + fibo2;
+            fibo1 = fibo2 - fibo1;
+        };
+        
+        //instalar nodemon con: npm install -g nodemon
+        
+        //a continuación se generará código para guardar el resultado en un archivo
+        fs.writeFile('fibonacci.txt', serie, (err)=>{
+            if (err)
+                reject("Error al crear el archivo");
+            else 
+                resolve("Archivo creado con exito!");
+        });  
+    });
+};
+
+module.exports = {
+    crearSerie
+};
