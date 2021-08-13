8e9915a21baa5c18297a3eaa5fd70ec431bd9075
Jorge Soltec <jdeleon@corposoltec.com>
2021-08-13 12:30:48 -0600

    se agregó el modelo de usuario y se agrego funcionalidad de agregar usuarios a mongodb

--- 07-APIRest-MongoDB/app.js
+++ 07-APIRest-MongoDB/app.js
@@ -1,11 +1,18 @@
+const usuarios = require("./routes/usuarios");
 const express = require('express');
 const mongoose = require('mongoose');
+
 
 mongoose.connect('mongodb://localhost:27017/demo',  {useUnifiedTopology: true, useNewUrlParser: true})
     .then(()=> console.log("conectado a MongoDB"))
     .catch(err => console.log("no se pudo conectar con MongoDB..."))
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api/usuarios', usuarios)
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(3000, ()=>{
